550b9a7fa8a66d61018a1cb84b8b93b9ad858467
ivankusuma07 <ivankusuma07@gmail.com>
2024-01-20 07:14:35 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+<template>
+  <div>
+    <p>This is where our header component will be </p>
+    <slot />
+    <p>This is where our footer component will be </p>
+  </div>
+</template>
